e2195c29b14487c3ad840d20903ec99ae47ad1b4
GairikSharma <88035292+GairikSharma@users.noreply.github.com>
2022-02-22 21:16:27 +0530

    Add files via upload

---
+++ hideandshow.js
@@ -0,0 +1,8 @@
+var content = document.getElementsByClassName('main')[0]
+        const hideandshow = ()=>{
+            if(content.style.display == "block"){
+                content.style.display = "none"
+            }else{
+                content.style.display = "block"
+            }
+        }
