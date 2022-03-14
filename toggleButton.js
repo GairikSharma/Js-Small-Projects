902595869bca36b917bfe0352c3d9df00a491c48
GairikSharma <88035292+GairikSharma@users.noreply.github.com>
2022-03-14 22:38:25 +0530

    Add files via upload

---
+++ toggleButton.js
@@ -0,0 +1,14 @@
+var left = document.getElementsByClassName("left")[0];
+// var show = false;
+const showLeft = () => {
+    if(left.style.display == "none"){
+        left.style.display = "block";
+    }
+    else{
+        left.style.display = "none"
+    }
+}
+
+
+
+    
